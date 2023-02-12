61b7a88fac7700eb1cabe64db82ed5969fdd0521
seml404 <4040591@gmail.com>
2023-02-12 21:56:03 +0400

    updated file structure of the project; added prompt service

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 // import { inquirer } from 'inquirer';
-
+import { PromptService } from "./core/prompt/prompt.service";
 export class App {
-    run (){
-        console.log('done');
-    }
+ async run() {
+    const res = await (new PromptService()).input<number>(
+      "Введите число",
+      "input"
+    );
+    console.log(res);
+  }
 }
 
 const app = new App();
